fa8a4a590599a1ec131c49f2c7c333cc469326bb
MAHAM HASSAN <161006055+mahamhassan12@users.noreply.github.com>
2024-04-14 13:49:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = ["maham", "aliza"];
+async function createtodo(arr) {
+    do {
+        let ans = await inquirer.prompt({
+            type: "list",
+            message: "Select an opeartion ",
+            name: "Select",
+            choices: ["Add", "Update", "View", "Delete"]
+        });
+        if (ans.Select === "Add") {
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "WHAT DO YOU WANT TO ADD ?",
+                name: "addtodo"
+            });
+            arr.push(addTodo.addtodo);
+            console.log(todos);
+        }
+        if (ans.Select === "Update") {
+            let updateTodo = await inquirer.prompt({
+                type: "list",
+                message: "WHAT DO YOU WANT TO UPDATE?",
+                name: "updtodo",
+                choices: todos.map(item => item)
+            });
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "WHAT DO YOU WANT TO ADD ?",
+                name: "addtodo"
+            });
+            let newTodo = todos.filter(val => val !== updateTodo.updtodo);
+            todos = [...newTodo, addTodo.addtodo];
+            console.log(todos);
+        }
+        if (ans.Select === "View") {
+            console.log(todos);
+        }
+        if (ans.Select === "Delete") {
+            let deleteTodo = await inquirer.prompt({
+                type: "list",
+                message: "WHAT DO YOU WANT TO UPDATE?",
+                name: "updtodo",
+                choices: todos.map(item => item)
+            });
+            let newTodo = todos.filter(val => val !== deleteTodo.updtodo);
+            todos = [...newTodo];
+            console.log(todos);
+        }
+    } while (true);
+}
+createtodo(todos);
